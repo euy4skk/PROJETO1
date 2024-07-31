42c6e436ca070495e1ea87de824fe9a2956a98c9
euy4skk <00001088941424sp@al.educacao.sp.gov.br>
2024-07-31 14:53:37 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas1(1000 , 800
+   background("purple");
+}
+
+function draw() {
+  
+ 
+  fill("#7D3AF5")
+  stroke("#F14E85")
+  
+ if (mouseIsPressed){  
+  circle(mouseX,mouseY,30);
+  }
+}
+
